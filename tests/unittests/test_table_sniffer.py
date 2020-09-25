fa278c79195b796db4c18af85cf53de3a63bf531
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-09-25 21:59:38 +0200

    style: Whitespace change

--- tests/unittests/test_table_sniffer.py
+++ tests/unittests/test_table_sniffer.py
@@ -10,4 +10,5 @@ def test_as_text():
     el = namedtuple("element", "text")
     html_elements = [el(text="foo"), el(text="bar"), el(text="buzz")]
     text_items = as_text(html_elements)
+
     assert text_items == ["foo", "bar", "buzz"]
