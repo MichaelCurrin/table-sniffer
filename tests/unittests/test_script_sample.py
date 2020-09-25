fb51a1aadf57367e4c4d6b96ec91f74d7ce4b60e
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-09-25 15:17:51 +0200

    test: Update test_script_sample.py

--- tests/unittests/test_script_sample.py
+++ tests/unittests/test_script_sample.py
@@ -1,13 +1,13 @@
 """
 Unit tests module.
 """
-from pyproject.script_sample import foo, bar
+# from table_sniffer.table_sniffer import ...
 
 
-def test_foo():
-    assert foo() is None
+# def test_foo():
+#     assert foo() is None
 
 
-def test_bar():
-    assert bar(0, 0) == 0
-    assert bar(5, 8) == 13
+# def test_bar():
+#     assert bar(0, 0) == 0
+#     assert bar(5, 8) == 13
