dd9a41e8097515117d713be1b1343c56006ff2b7
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-09-25 21:24:20 +0200

    refactor: Remove types

--- table_sniffer/etc/constants.py
+++ table_sniffer/etc/constants.py
@@ -4,9 +4,9 @@ Constants module.
 from pathlib import Path
 
 
-APP_DIR: Path = Path(__file__).resolve().parent.parent
-VAR_DIR: Path = APP_DIR / "var"
-WIKI_TABLE_CLASS: str = "wikitable"
+APP_DIR = Path(__file__).resolve().parent.parent
+VAR_DIR = APP_DIR / "var"
+WIKI_TABLE_CLASS = "wikitable"
 
-WIKI_DOMAIN: str = "wikipedia.org"
-DEFAULT_URL: str = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+WIKI_DOMAIN = "wikipedia.org"
+DEFAULT_URL = "https://en.wikipedia.org/wiki/Python_(programming_language)"
